f32d83e0d19878b8e004a75f323620d372f9e7ca
Pat O'Connor <paoconno@redhat.com>
2026-09-10 16:01:55 +0100

    register apiextensions in main.go
    Signed-off-by: Pat O'Connor <paoconno@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -24,6 +24,7 @@ import (
 
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -46,6 +47,7 @@ var (
 
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(apiextensionsv1.AddToScheme(scheme))
 
 	utilruntime.Must(componentsv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
